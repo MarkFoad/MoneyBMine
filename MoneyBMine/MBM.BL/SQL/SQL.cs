98b61a045f07ac9cc8dfec2c2ad3bff3d2892456
MarkFoad <markf@dahallco.com.au>
2019-08-18 12:08:06 +1000

    Added Model Stock to hold the Stock Exchange Records Created the SQL AddRecord Command

---
+++ MoneyBMine/MBM.BL/SQL/SQL.cs
@@ -0,0 +1,61 @@
+﻿using MBM.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBM.BL.SQL
+{
+    public class SQL
+    {
+        /// <summary>
+        /// Sets the SQL Connection string
+        /// </summary>
+        protected static string sqlConnection = @"Data Source=.\;Initial Catalog=MoneyBMine;Persist Security Info=True;User ID=sa;Password=admin";
+
+        /// <summary>
+        /// Gets or sets the- table name to be used.
+        /// </summary>
+        public string TableName { get; set; }
+
+        /// <summary>
+        /// Adds a new record to the SQL database
+        /// </summary>
+        public async void AddRecord()
+        {
+            Stock stock = new Stock();
+            SqlConnection connection = new SqlConnection(sqlConnection);
+            connection.Open();
+            //Building the query sting and parameters
+            string query = $"INSERT INTO [MoneyBMine].[dbo].[{TableName}] ([StockExchange], [StockSymbol], [Date], [StockPriceOpen], [StockPriceHigh], [StockPriceLow], [StockPriceClose], [StockVolume], [StockPriceAdjClose]) ";
+            query += " Values (@StockExchange, @StockSymbol, @Date, @StockPriceOpen, @StockPriceHigh,@StockPriceLow, @StockPriceClose, @StockVolume, @StockPriceAdjClose)";
+
+            if (connection.State == ConnectionState.Open)
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@StockExchange", stock.StockExchange);
+                command.Parameters.AddWithValue("@StockSymbol", stock.StockSymbol);
+                command.Parameters.AddWithValue("@Date", stock.Date);
+                command.Parameters.AddWithValue("@StockPriceOpen", stock.StockPriceOpen);
+                command.Parameters.AddWithValue("@StockPriceHigh", stock.StockPriceHigh);
+                command.Parameters.AddWithValue("@StockPriceLow", stock.StockPriceLow);
+                command.Parameters.AddWithValue("@StockPriceClose", stock.StockPriceClose);
+                command.Parameters.AddWithValue("@StockVolume", stock.StockVolume);
+                command.Parameters.AddWithValue("@StockPriceAdjClose", stock.StockPriceAdjClose);
+                await command.ExecuteNonQueryAsync();
+
+            }
+            else
+            {
+                throw new Exception("Connection to Database failed");
+                   
+            }
+            // Makes sure the connection is closed on completion.
+            connection.Close();
+
+        }
+    }
+}
---
+++ MoneyBMine/MBM.WPF.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MBM.WPF.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MBM.WPF.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02ed32d4-159c-4a59-a7c2-c2576aeb6283")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

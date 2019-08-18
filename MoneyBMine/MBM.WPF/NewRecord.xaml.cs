296b30de733c85455cd5595906b5c85e6bf1bbf2
MarkFoad <markf@dahallco.com.au>
2019-08-18 16:17:41 +1000

    Added the close window function tot eh Cancel button Added XML commenting

--- MoneyBMine/MBM.WPF/NewRecord.xaml.cs
+++ MoneyBMine/MBM.WPF/NewRecord.xaml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,8 +23,14 @@ namespace MBM.WPF
     /// </summary>
     public partial class NewRecord : Window
     {
-        
+        /// <summary>
+        /// Instantiates a new instance of the SQL repository
+        /// </summary>
         SQL sql = new SQL();
+
+        /// <summary>
+        /// Initializes the New Record window.
+        /// </summary>
         public NewRecord()
         {
             InitializeComponent();
@@ -32,6 +39,11 @@ namespace MBM.WPF
 
         }
 
+        /// <summary>
+        /// Message to display when successfully written to the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddCompletedSuccessEvent(object sender, EventArgs e)
         {
             MessageBox.Show("Record added successfully");
@@ -45,24 +57,63 @@ namespace MBM.WPF
         /// <param name="e"></param>
         private void FailedUpdateEvent(object sender, EventArgs e)
         {
-            
+
         }
 
+        /// <summary>
+        /// Saves the record to the SQL database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnSubmit_Click(object sender, RoutedEventArgs e)
         {
             Stock stock = new Stock();
+            if (tbStockExchange.Text != null)
+            {
+                stock.StockExchange = tbStockExchange.Text.ToUpper();
+            }
+            if (tbStockSymbol.Text != null)
+            {
+                stock.StockSymbol = tbStockSymbol.Text.ToUpper();
+            }
+            if (dpDate.SelectedDate.Value != null)
+            {
 
-            stock.StockExchange = tbStockExchange.Text.ToUpper();
-            stock.StockSymbol = tbStockSymbol.Text.ToUpper();
-            stock.Date = dpDate.SelectedDate.Value;
-            stock.StockPriceOpen = double.Parse(tbStockPriceOpen.Text);
+                stock.Date = dpDate.SelectedDate.Value;
+            }
+            if (tbStockPriceOpen.Text != null)
+            {
+
+                stock.StockPriceOpen = double.Parse(tbStockPriceOpen.Text);
+            }
             stock.StockPriceHigh = double.Parse(tbStockPriceHigh.Text);
             stock.StockPriceLow = double.Parse(tbStockPriceLow.Text);
             stock.StockPriceClose = double.Parse(tbStockPriceClose.Text);
             stock.StockVolume = int.Parse(tbStockVolume.Text);
             stock.StockPriceAdjClose = double.Parse(tbStockPriceAdjClose.Text);
             sql.AddRecord(stock);
-            
+
         }
-    }
+        /// <summary>
+        /// Checks that the entered value is numeric
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // Copied from the MSDN Site for WPF Text-box with Decimal numeric value
+            Regex regex = new Regex("^[.][0-9]+$|^[0-9]*[.]{0,1}[0-9]*$");
+            e.Handled = !regex.IsMatch((sender as TextBox).Text.Insert((sender as TextBox).SelectionStart, e.Text));
+        }
+
+        /// <summary>
+        /// Closes the Add New Record window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }    
 }
